138e58e56ae784961de15d453910b9920ab728d4
KrystianJonca <33162450+KrystianJonca@users.noreply.github.com>
2018-10-06 14:31:15 +0200

    Create Header
    Create Header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
   <div id="app">
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Header
+  }
 }
 </script>
 
 <style lang="less">
+body{
+  font-family: 'Open Sans', sans-serif;
+  background-color: #212121;
+  height: 100%;
+  color: #FFFFFF;
+}
+.container{
+  margin: auto;
+  width: 50%;
+  @media (max-width: 1025px) { width: 75% }
+  @media (max-width: 767px) { width: 100% }
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,8 @@
 <template>
-
+  <div class="container">
+    <h1>Krystian 'Wionek' Jońca</h1>
+    <p>Node.js developer from Poland</p>
+  </div>
 </template>
 
 <script>
@@ -9,5 +12,17 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.container{
+  border-bottom: 1px solid #FFFFFF;
+  h1{
+    margin-top: 10%;
+    margin-bottom: 0;
+    text-align: center;
+  }
+  p{
+    color: #E0E0E0;
+    margin-top: 0;
+    text-align: center;
+  }
+}
 </style>

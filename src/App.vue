f07bbf30975172b4822972f4705c2f16559f6b53
KrystianJonca <33162450+KrystianJonca@users.noreply.github.com>
2018-10-06 15:24:27 +0200

    Add main content
    Add main content

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Header />
+    <Main />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Main from './components/Main'
 
 export default {
   name: 'app',
   components: {
-    Header
+    Header,
+    Main
   }
 }
 </script>
@@ -24,8 +27,12 @@ body{
 }
 .container{
   margin: auto;
-  width: 50%;
+  width: 40%;
   @media (max-width: 1025px) { width: 75% }
   @media (max-width: 767px) { width: 100% }
 }
+p{
+  color: #E0E0E0;
+}
+
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,44 @@
 <template>
-
+  <div class="container">
+    <p>
+      Hi! My name is Krystian 'Wionek' Jonca. I am 15 years old developer from Poland.
+      I started programming when i was 13 now I am working on my open-source projects in Node.js.
+      You can view some fo them in my <a target="_blank" href="https://www.gitshowcase.com/krystianjonca">
+      <strong>Portfolio</strong>💪</a>
+    </p>
+    <p>
+      If you heard about my modules and you like them, please consider buying me coffee. Your support will help me doing more open-source projects for you!
+    </p>
+    <div class="btn">
+      <link href="https://fonts.googleapis.com/css?family=Cookie" rel="stylesheet"><a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/wionek"><img src="https://www.buymeacoffee.com/assets/img/BMC-btn-logo.svg" alt="Buy me a coffee"><span style="margin-left:5px">Buy me a coffee</span></a>
+    </div>
+    <p class="center"><strong>Thank you for your support!</strong>❤️</p>
+ </div>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Main'
 }
 </script>
 
 <style lang="less" scoped>
+.center{
+  text-align: center !important;
+}
+p{
+  margin: 30px;
+  text-align: justify;
+}
+.btn{
+  margin: auto;
+  width: 173px;
+}
+a {
+  text-decoration: none;
+  color: #FFFFFF;
+}
 
+// Buy me a coffe button
+.bmc-button img{width: 27px !important;margin-bottom: 1px !important;box-shadow: none !important;border: none !important;vertical-align: middle !important;}.bmc-button{line-height: 36px !important;height:37px !important;text-decoration: none !important;display:inline-flex !important;color:#000000 !important;background-color:#FFFFFF !important;border-radius: 3px !important;border: 1px solid transparent !important;padding: 1px 9px !important;font-size: 23px !important;letter-spacing: 0.6px !important;box-shadow: 0px 1px 2px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;margin: 0 auto !important;font-family:'Cookie', cursive !important;-webkit-box-sizing: border-box !important;box-sizing: border-box !important;-o-transition: 0.3s all linear !important;-webkit-transition: 0.3s all linear !important;-moz-transition: 0.3s all linear !important;-ms-transition: 0.3s all linear !important;transition: 0.3s all linear !important;}.bmc-button:hover, .bmc-button:active, .bmc-button:focus {-webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;text-decoration: none !important;box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;opacity: 0.85 !important;color:#000000 !important;}
 </style>
